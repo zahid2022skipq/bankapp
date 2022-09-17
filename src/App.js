038711b316cc59454972efe56edda1e6150d3422
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-09-17 17:57:12 +0500

    Designing done

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
 import "./App.css";
 import data from "./utils/Data_Users";
-import { Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import Transaction from "./Transaction";
 
 function App() {
   return (
-    <div className="App">
-      <table>
-        <tbody>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Balance</th>
-          </tr>
-          {data.map((d) => (
-            <tr key={d.id}>
-              <td>{d.name}</td>
-              <td>{d.email}</td>
-              <td>{d.currentBalance}</td>
+    <>
+      <div className="App">
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Balance</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-
-      <div>
-        <Link>Transfer Funds</Link>
+            {data.map((d) => (
+              <tr key={d.id}>
+                <td>{d.name}</td>
+                <td>{d.email}</td>
+                <td>{d.currentBalance}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div>
+          <Link to="transaction">Transfer Funds</Link>
+        </div>
       </div>
-    </div>
+
+      <Routes>
+        <Route exact path="/*" element={<App />} />
+        <Route exact path="/transaction" element={<Transaction />} />
+      </Routes>
+    </>
   );
 }
 
